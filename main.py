d2995ccc2f7e00c29294f8b9509c9fb83747a5c2
IsmailBl <ismailboulahnine@gmail.com>
2024-01-12 11:05:57 +0100

    Upload of the main node

---
+++ main.py
@@ -0,0 +1,22 @@
+#Standard Libraries import
+import matplotlib.pyplot as plt
+import numpy as np
+import random as rnd
+def grid_gen():
+    x = rnd.randint(0, 9)
+    print (x)
+    grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+    
+    print(grid)
+
+if __name__ == '__main__':
+    grid_gen()
+
---
+++ test.py
@@ -0,0 +1,19 @@
+operateur = input("Please Enter your operator : ")
+if (operateur == "+"):
+    a = float(input ("Enter a : "))
+    b = float(input ("Enter b : "))
+    print("The result is : ",(a+b))
+elif (operateur == "//"):
+    a = float(input ("Enter a : "))
+    b = float(input ("Enter b : "))
+    print("The result is : ",(a//b))
+
+elif (operateur == "-"):
+    a = float(input ("Enter a : "))
+    b = float(input ("Enter b : "))
+    print("The result is : ",(a-b))
+
+elif (operateur == "*"):
+    a = float(input ("Enter a : "))
+    b = float(input ("Enter b : "))
+    print("The result is : ",(a*b))
