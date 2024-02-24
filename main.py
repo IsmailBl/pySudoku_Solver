0f05ff723b43cb8260ab56c029701ef5bed62dde
ismail-boulahnine <ismail.boulahnine@fairmat.tech>
2024-02-24 13:38:57 +0100

    Fix::Array

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random as rnd
 def grid_gen():
     x = rnd.randint(0, 9)
     print (x)
-    grid = np.array(([1,9,0,7,0,5,0,0,0],[6,5,0,0,0,1,4,0,7], ))
+    grid = np.array(([1,9,0,7,0,5,0,0,0],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7] ))
     shape = grid.shape
     print(f"Shape is : {shape}") 
     return (grid)
@@ -13,4 +13,3 @@ def check_grid(grid):
     pass
 if __name__ == '__main__':
     grid =grid_gen()
-  
