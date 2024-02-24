e24a32d044634950d506020b33e06e701b16ccd3
ismail-boulahnine <ismail.boulahnine@fairmat.tech>
2024-02-24 13:30:38 +0100

    Feat:Main code fixedDD

--- main.py
+++ main.py
@@ -5,18 +5,12 @@ import random as rnd
 def grid_gen():
     x = rnd.randint(0, 9)
     print (x)
-    grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-        [5, 2, 0, 0, 0, 0, 0, 0, 0],
-        [0, 8, 7, 0, 0, 0, 0, 3, 1],
-        [0, 0, 3, 0, 1, 0, 0, 8, 0],
-        [9, 0, 0, 8, 6, 3, 0, 0, 5],
-        [0, 5, 0, 0, 9, 0, 6, 0, 0],
-        [1, 3, 0, 0, 0, 0, 2, 5, 0],
-        [0, 0, 0, 0, 0, 0, 0, 7, 4],
-        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
-    
-    print(grid)
-
+    grid = np.array(([1,9,0,7,0,5,0,0,0],[6,5,0,0,0,1,4,0,7], ))
+    shape = grid.shape
+    print(f"Shape is : {shape}") 
+    return (grid)
+def check_grid(grid):
+    pass
 if __name__ == '__main__':
-    grid_gen()
-
+    grid =grid_gen()
+  
