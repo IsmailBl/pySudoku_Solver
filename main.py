036e72bf693338003af5b28b0825ffb42e11ec46
IsmailBl <ismailboulahnine@gmail.com>
2024-03-13 03:11:11 +0100

    Feat: Grid's solvability function added

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-#Standard Libraries import
+"""This project is a 9x9G Grid Sdoku solving program based on a backtracking
+algorithm. 
+"""
+__version__ = '0.1'
+__author__ = 'Ismail Boulahnine'
+
+from __future__ import barry_as_FLUFL
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rnd
 def grid_gen():
     x = rnd.randint(0, 9)
     print (x)
-    grid = np.array(([1,9,0,7,0,5,0,0,0],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7] ))
+    grid = np.array(([1,9,0,7,0,5,0,0,0],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,0],[6,5,0,0,0,1,4,0,0],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7] ))
     shape = grid.shape
     print(f"Shape is : {shape}") 
     return (grid)
@@ -19,6 +25,14 @@ def check_grid(grid):
             if grid [x][y] == 0: 
                 zeros = zeros +1
     return (print (f'Number of zeros is : {zeros}'))
+def print_grid(grid):
+    shape = grid.shape
+    row = shape[0]
+    col = shape[1]
+    for i in range(row):
+        for j in range (col):
+            print(f' ', grid[j][i])
 if __name__ == '__main__':
     grid =grid_gen()
     check_grid(grid)
+    print(grid)
