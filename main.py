ecadbac69bf262eb26bd5b2b545fd8d06aa5795b
IsmailBl <ismailboulahnine@gmail.com>
2024-03-17 16:52:55 +0100

    Fix: Grid validity function

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-"""This project is a 9x9G Grid Sdoku solving program based on a backtracking
-algorithm. 
-"""
-__version__ = '0.1'
-__author__ = 'Ismail Boulahnine'
-
 from __future__ import barry_as_FLUFL
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rnd
+"""This project is a 9x9G Grid Sdoku solving program based on a backtracking
+algorithm. 
+"""
+__version__ = '0.1'
+__author__ = 'Ismail B.'
+
 def grid_gen():
     x = rnd.randint(0, 9)
     print (x)
@@ -25,14 +25,18 @@ def check_grid(grid):
             if grid [x][y] == 0: 
                 zeros = zeros +1
     return (print (f'Number of zeros is : {zeros}'))
-def print_grid(grid):
-    shape = grid.shape
-    row = shape[0]
-    col = shape[1]
-    for i in range(row):
+
+def is_valid(grid, num,  row= 9 , col=9):
+    x = 0 
+    for i in range (row):
         for j in range (col):
-            print(f' ', grid[j][i])
+            if grid[i][j] == num :
+                x += 1
+            if x > 1 : 
+                print ('Grid is unsolveable !')
+                break 
 if __name__ == '__main__':
     grid =grid_gen()
     check_grid(grid)
     print(grid)
+    is_valid (grid, 7)
