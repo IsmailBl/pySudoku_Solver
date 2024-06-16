d6adb9bdc0e94ee7e4689a8cd9e354a4ac8e6a36
IsmailBl <ismailboulahnine@gmail.com>
2024-06-16 03:04:28 +0200

    Feat : New implementation

--- main.py
+++ main.py
@@ -8,35 +8,21 @@ algorithm.
 __version__ = '0.1'
 __author__ = 'Ismail B.'
 
-def grid_gen():
-    x = rnd.randint(0, 9)
-    print (x)
-    grid = np.array(([1,9,0,7,0,5,0,0,0],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,0],[6,5,0,0,0,1,4,0,0],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7],[6,5,0,0,0,1,4,0,7] ))
-    shape = grid.shape
-    print(f"Shape is : {shape}") 
-    return (grid)
-def check_grid(grid):
-    shape = grid.shape
-    i = shape [0]
-    j = shape [1]
-    zeros = 0 
-    for x in range(i):
-        for y in range(j): 
-            if grid [x][y] == 0: 
-                zeros = zeros +1
-    return (print (f'Number of zeros is : {zeros}'))
+my_grid = np.array([[1,2,3],
+                    [1,2,3]])
 
-def is_valid(grid, num,  row= 9 , col=9):
-    x = 0 
-    for i in range (row):
-        for j in range (col):
-            if grid[i][j] == num :
-                x += 1
-            if x > 1 : 
-                print ('Grid is unsolveable !')
-                break 
-if __name__ == '__main__':
-    grid =grid_gen()
-    check_grid(grid)
-    print(grid)
-    is_valid (grid, 7)
+class gridSolver ():
+    def __init__ (self, grid) -> None : 
+        self.grid = grid
+        self.size =[]
+    def grid_size (self):
+        m , n = np.shape(self.grid)
+        self.size.append(m)
+        self.size.append(n)
+        return self.size 
+    
+    def check_solv(self, grid):
+        pass     
+if __name__ == "__main__":
+    grid_slv = gridSolver(my_grid)
+    print (grid_slv.grid_size())
